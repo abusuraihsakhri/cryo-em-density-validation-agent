17eed40ec0309f7327a6f3ca756d8804ddf0d3fa
abusuraihsakhri <abusuraihsakhri@gmail.com>
2026-08-26 00:00:03 +0530

    Initial Commit

---
+++ cryo_em_validator/__init__.py
@@ -0,0 +1,6 @@
+"""
+CryoEM-Validator: Fourier Shell Correlation (FSC) & Local Resolution Agent
+Domain: Biomedical Scientific Discovery & Multi-Omics
+Standard: wwPDB / EMDataBank Cryo-EM Standards
+"""
+__version__ = "2.0.0-FRONTIER"
---
+++ cryo_em_validator_app.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import sys
+from cryo_em_validator.cli import main
+
+if __name__ == '__main__':
+    sys.exit(main())
